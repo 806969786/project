909ef6ef12c960537a358707f859f8b8c8cc9e56
xmcc <xmcc@163.com>
2019-06-19 16:49:47 +0800

    项目搭建

---
+++ sell_project/src/main/java/com/xmcc/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.xmcc.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/say")
+    public String test(){
+        return "hello";
+    }
+
+}
